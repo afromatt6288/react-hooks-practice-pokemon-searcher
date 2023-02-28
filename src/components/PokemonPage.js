bf8792e7a8eaffcc9c3f93101a409e0757d6182c
Matthew Clark <afromatt6288@hotmail.com>
2023-02-28 14:43:04 -0700

    Lab Complete. Added the beginings of a delete function as well.

--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -1,19 +1,37 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import PokemonCollection from "./PokemonCollection";
 import PokemonForm from "./PokemonForm";
 import Search from "./Search";
 import { Container } from "semantic-ui-react";
 
 function PokemonPage() {
+  const [pokemon, setPokemon] = useState([])
+  const [search, setSearch] = useState("")
+
+  useEffect(()=> {
+    fetch("http://localhost:3001/pokemon")
+    .then(r => r.json())
+    .then(poke => setPokemon(poke))
+  },[])
+  console.log(pokemon)
+
+  function handleAddPokemon(pokeData) {
+    const newPokemon = [...pokemon, pokeData]
+    setPokemon(newPokemon)
+  }
+
+  // this is how I am handling the Search function
+  const displayedPokemon = pokemon.filter(poke => poke.name.toLowerCase().includes(search.toLowerCase()))
+
   return (
     <Container>
       <h1>Pokemon Searcher</h1>
       <br />
-      <PokemonForm />
+      <PokemonForm onAddPokemon={handleAddPokemon}/>
       <br />
-      <Search />
+      <Search search={search} onSearchChange={setSearch}/>
       <br />
-      <PokemonCollection />
+      <PokemonCollection pokemon={displayedPokemon}/>
     </Container>
   );
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-function Search() {
+function Search({search, onSearchChange}) {
   return (
     <div className="ui search">
       <div className="ui icon input">
-        <input className="prompt" />
+        <input className="prompt" type="text" name="search" placeholder="Search..." value={search} onChange={e=> onSearchChange(e.target.value)}/>
         <i className="search icon" />
       </div>
     </div>
